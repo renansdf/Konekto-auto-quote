1f4a38ad38136c71539fa898cb4d4696b141db92
renansdf <rnn.frts@gmail.com>
2020-12-17 15:32:44 -0300

    created header for application with a title

---
+++ src/components/header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from './styles';
+
+const Header: React.FC = () => (
+  <Container>
+    <h1>Konekto Auto Quote</h1>
+  </Container>
+);
+
+export default Header;
---
+++ src/components/header/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  background: linear-gradient(123deg,#15008a,#5347d5);
+
+  padding: 10px 0px;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 40px;
+
+  h1{
+    margin: 0px;
+    color: #fff;
+  }
+`;

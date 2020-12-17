17b8a05877844657dfee096bb9ad0cc0fc3a5970
renansdf <rnn.frts@gmail.com>
2020-12-17 15:28:57 -0300

    added new components and styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Header from './pages/header';
+import Footer from './pages/footer';
+
+import Quote from './pages/quote';
+import Sidebar from './pages/sidebar';
+
+import { Container } from './styles/AppStyles';
 
 const App: React.FC = () => (
-  <h1>Orçamento Konekto</h1>
+  <>
+    <Header />
+    <Container>
+      <Quote />
+      <Sidebar />
+    </Container>
+    <Footer />
+  </>
 );
 
 export default App;

8a3fcfdca81a04d991abdb634875b705e2f68adb
renansdf <rnn.frts@gmail.com>
2020-12-17 15:28:30 -0300

    created Global container styles and adjustments

---
+++ src/styles/AppStyles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  width: 90%;
+  max-width: 1000px;
+  margin: 0 auto;
+`;
--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,17 +1,24 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  *{
+    box-sizing: border-box;
+  }
+
   body{
     background: #f5f4ff;
     font-family: 'Mulish';
     color: #483cc9;
     letter-spacing: 1px;
+
+    margin: 0px;
   }
 
   h1, h2, h3, h4, h5, h6{
     font-family: 'Castoro';
     font-style: italic;
     font-weight: 400;
+    margin: 0px 0 20px;
   }
 `;
 

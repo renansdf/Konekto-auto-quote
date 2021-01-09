44d126f87648732091abe37a23d887e1674e2e74
renansdf <rnn.frts@gmail.com>
2021-01-08 21:04:09 -0300

    added select options from language component and refactored the handle update function

--- src/components/FormLanguageMinutes/index.tsx
+++ src/components/FormLanguageMinutes/index.tsx
@@ -6,15 +6,18 @@ import Select from '../Select';
 import { FormContainer, SectionButton, GoBackButton } from '../../styles/AppStyles';
 import { useQuoteData } from '../../hooks/quoteData';
 
+import allLanguages from '../../helpers/languages';
+
 interface IServiceFormProps {
   isVisible: boolean;
   toggleVisibility: () => void;
   goBackButton: () => void;
 }
 
-interface SubmittedData {
+interface IFormData {
   languageMatrix: string;
   languageFinal: string;
+  languageGroup: number;
   totalMinutes: number;
 }
 
@@ -28,20 +31,40 @@ const FormLanguageMinutes: React.FC<IServiceFormProps> = ({
 
   const handleUpdate = () => {
     if (formRef.current) {
-      const formData: SubmittedData = formRef.current.getData();
-      if (serviceData && serviceData.service) {
-        setServiceData({ ...formData, service: serviceData.service });
+      const formData: IFormData = formRef.current.getData();
+
+      const matrixData = formData.languageMatrix.split(',');
+      const matrixNumber = Number(matrixData[0]);
+      const matrixLanguage = matrixData[1];
+
+      const finalData = formData.languageFinal.split(',');
+      const finalNumber = Number(finalData[0]);
+      const finalLanguage = finalData[1];
+
+      let selectedGroup: number;
+      if (matrixNumber >= finalNumber) {
+        selectedGroup = matrixNumber;
       } else {
-        setServiceData({ ...formData, service: '' });
+        selectedGroup = finalNumber;
       }
+
+      setServiceData({
+        languageFinal: finalLanguage,
+        languageMatrix: matrixLanguage,
+        totalMinutes: formData.totalMinutes,
+        languageGroup: selectedGroup,
+        service: '',
+      });
     }
   };
 
-  const handleSubmit = (data: SubmittedData) => {
-    if (serviceData && serviceData.service) {
-      setServiceData({ ...data, service: serviceData.service });
-    } else {
-      setServiceData({ ...data, service: '' });
+  const handleSubmit = () => {
+    if (serviceData) {
+      if (serviceData.service) {
+        setServiceData({ ...serviceData, service: serviceData.service });
+      } else {
+        setServiceData({ ...serviceData, service: '' });
+      }
     }
     toggleVisibility();
   };
@@ -54,14 +77,26 @@ const FormLanguageMinutes: React.FC<IServiceFormProps> = ({
 
         <Select name="languageMatrix" onChange={handleUpdate}>
           <option value="">Lingua fonte</option>
-          <option value="Português">Português</option>
-          <option value="Inglês">Inglês</option>
+          {allLanguages && allLanguages.map((language) => (
+            <option
+              key={language.name}
+              value={[language.group.toString(), language.name]}
+            >
+              {language.name}
+            </option>
+          ))}
         </Select>
 
         <Select name="languageFinal" onChange={handleUpdate}>
           <option value="">Lingua final</option>
-          <option value="Português">Português</option>
-          <option value="Inglês">Inglês</option>
+          {allLanguages && allLanguages.map((language) => (
+            <option
+              key={language.name}
+              value={[language.group.toString(), language.name]}
+            >
+              {language.name}
+            </option>
+          ))}
         </Select>
 
         <SectionButton type="submit">Selecione os detalhes do serviço</SectionButton>
--- src/components/FormLanguageWords/index.tsx
+++ src/components/FormLanguageWords/index.tsx
@@ -6,15 +6,18 @@ import Select from '../Select';
 import { FormContainer, SectionButton, GoBackButton } from '../../styles/AppStyles';
 import { useQuoteData } from '../../hooks/quoteData';
 
+import allLanguages from '../../helpers/languages';
+
 interface IServiceFormProps {
   isVisible: boolean;
   toggleVisibility: () => void;
   goBackButton: () => void;
 }
 
-interface SubmittedData {
+interface IFormData {
   languageMatrix: string;
   languageFinal: string;
+  languageGroup: number;
   numberOfWords: number;
 }
 
@@ -23,18 +26,46 @@ const FormLanguageWords: React.FC<IServiceFormProps> = ({
   toggleVisibility,
   goBackButton,
 }) => {
-  const { setServiceData } = useQuoteData();
+  const { setServiceData, serviceData } = useQuoteData();
   const formRef = useRef<FormHandles>(null);
 
   const handleUpdate = () => {
     if (formRef.current) {
-      const formData: SubmittedData = formRef.current.getData();
-      setServiceData({ ...formData, service: '' });
+      const formData: IFormData = formRef.current.getData();
+
+      const matrixData = formData.languageMatrix.split(',');
+      const matrixNumber = Number(matrixData[0]);
+      const matrixLanguage = matrixData[1];
+
+      const finalData = formData.languageFinal.split(',');
+      const finalNumber = Number(finalData[0]);
+      const finalLanguage = finalData[1];
+
+      let selectedGroup: number;
+      if (matrixNumber >= finalNumber) {
+        selectedGroup = matrixNumber;
+      } else {
+        selectedGroup = finalNumber;
+      }
+
+      setServiceData({
+        languageFinal: finalLanguage,
+        languageMatrix: matrixLanguage,
+        numberOfWords: formData.numberOfWords,
+        languageGroup: selectedGroup,
+        service: '',
+      });
     }
   };
 
-  const handleSubmit = (data: SubmittedData) => {
-    setServiceData({ ...data, service: '' });
+  const handleSubmit = () => {
+    if (serviceData) {
+      if (serviceData.service) {
+        setServiceData({ ...serviceData, service: serviceData.service });
+      } else {
+        setServiceData({ ...serviceData, service: '' });
+      }
+    }
     toggleVisibility();
   };
 
@@ -46,14 +77,26 @@ const FormLanguageWords: React.FC<IServiceFormProps> = ({
 
         <Select name="languageMatrix" onChange={handleUpdate}>
           <option value="">Lingua fonte</option>
-          <option value="Português">Português</option>
-          <option value="Inglês">Inglês</option>
+          {allLanguages && allLanguages.map((language) => (
+            <option
+              key={`${language.name}-matrix`}
+              value={[language.group.toString(), language.name]}
+            >
+              {language.name}
+            </option>
+          ))}
         </Select>
 
         <Select name="languageFinal" onChange={handleUpdate}>
           <option value="">Lingua final</option>
-          <option value="Português">Português</option>
-          <option value="Inglês">Inglês</option>
+          {allLanguages && allLanguages.map((language) => (
+            <option
+              key={language.name}
+              value={[language.group.toString(), language.name]}
+            >
+              {language.name}
+            </option>
+          ))}
         </Select>
 
         <SectionButton type="submit">Selecione os detalhes do serviço</SectionButton>

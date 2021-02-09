a3fe9ae59bdb1d52dc7491f1ee9a5b294b2087c5
renansdf <rnn.frts@gmail.com>
2021-02-09 16:55:18 -0300

    added menu and cover components and styles

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container, Navbar, Cover } from './styles';
+import Logo from '../../images/logo-horizontal-preta.png';
 
 const Header: React.FC = () => (
   <Container>
-    <h1>Konekto Auto Quote</h1>
+    <Navbar>
+      <a href="https://www.konekto.me"><img src={Logo} alt="Konekto" /></a>
+      <div>
+        <a href="https://www.konekto.me/sobre">sobre</a>
+        <a href="https://www.konekto.me/servicos">serviços</a>
+        <a href="https://www.konekto.me/industrias">indústrias</a>
+        <a href="https://www.konekto.me/idiomas">idiomas</a>
+        <a href="https://www.konekto.me/idiomas">orçamento rápido</a>
+      </div>
+    </Navbar>
+
+    <Cover>
+      <h1>Orçamento Instantâneo</h1>
+    </Cover>
   </Container>
 );
 
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -1,16 +1,74 @@
 import styled from 'styled-components';
+import BGVerde from '../../images/forma-tr-verde.png';
+import BGRosa from '../../images/forma-bl-rosa.png';
 
 export const Container = styled.div`
   width: 100%;
-  background: linear-gradient(123deg,#15008a,#5347d5);
+  background: #fff;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 
-  padding: 10px 0px;
+  padding: 7px 0px;
+`;
+
+export const Navbar = styled.nav`
+  width: 100%;
+  max-width: 1000px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  position: absolute;
+  z-index: 2;
+
+  > a{
+    display: block;
+  }
+
+  img{
+    max-width: 120px;
+    margin: 0px;
+  }
+
+  div{
+    a{
+      padding: 5px 20px;
+      color: #000;
+      text-decoration: none;
+      font-size: 16px;
+      line-height: 1.2em;
+      font-weight: 300;
+      letter-spacing: .5px;
+    }
+
+    a:last-child{
+      font-weight: 700;
+    }
+  }
+`;
+
+export const Cover = styled.section`
   display: flex;
   justify-content: center;
-  margin-bottom: 40px;
+  align-items: center;
+  height: 60vh;
+  padding-top: 60px;
+  width: 100%;
+  position: relative;
+  z-index: 1;
+
+  background-image: url(${BGVerde}), url(${BGRosa});
+
+  background-repeat: no-repeat, no-repeat;
+  background-position: 100% 100%,0 100%;
+  background-size: auto 80%,auto 80%;
+
 
   h1{
-    margin: 0px;
-    color: #fff;
+    font-weight: 900;
+    font-size: 110px;
+    line-height: 1em;
+    text-align: center;
   }
 `;

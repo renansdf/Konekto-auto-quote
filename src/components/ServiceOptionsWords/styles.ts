e9b8e1da0956be229631e94fc0e547929152f287
renansdf <rnn.frts@gmail.com>
2021-03-04 21:58:43 -0300

    refactored components into text and timebased documents and products components

--- src/components/ServiceOptionsWords/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  margin-bottom: 15px;
-`;
